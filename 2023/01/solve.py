dd6c0e56c6ea874413a4a8d054efc4e7732f29d5
Jeppe Dakin <jeppe_dakin@hotmail.com>
2023-12-02 13:01:23 +0100

    rename main.py -> solve.py

---
+++ 2023/01/solve.py
@@ -0,0 +1,51 @@
+import re
+
+import pathlib, sys
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
+from magic import *
+
+
+# Reading in data
+@memoize
+def read():
+    with open_input_file() as file:
+        lines = [line.strip() for line in file]
+    return lines
+
+
+# Solution to part one
+@timer
+def solve_one(lines):
+    sum_calibration = 0
+    for line in lines:
+        m = re.search(r'\d', line)[0]
+        n = re.search(r'\d', line[::-1])[0]
+        calibration = int(m + n)
+        sum_calibration += calibration
+    return sum_calibration
+
+
+# Solution to part two
+@timer
+def solve_two(lines):
+    numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    values = {number: str(index) for index, number in enumerate(numbers, 1)}
+    values |= {number[::-1]: str(index) for index, number in enumerate(numbers, 1)}
+    pattern_digit = r'\d'
+    pattern_spelling = '|'.join(numbers)
+    pattern_forward = '{}|{}'.format(pattern_digit, pattern_spelling)
+    pattern_backward = '{}|{}'.format(pattern_digit, pattern_spelling[::-1])
+    regex_forward = re.compile(pattern_forward)
+    regex_backward = re.compile(pattern_backward)
+    sum_calibration = 0
+    for line in lines:
+        m = values.get(m := re.search(regex_forward, line)[0], m)
+        n = values.get(n := re.search(regex_backward, line[::-1])[0], n)
+        calibration = int(m + n)
+        sum_calibration += calibration
+    return sum_calibration
+
+
+# Solve
+print(solve_one(read()))
+print(solve_two(read()))
---
+++ 2023/02/solve.py
@@ -0,0 +1,59 @@
+import collections, math, re
+
+import pathlib, sys
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
+from magic import *
+
+
+# Reading in data
+@memoize
+def read():
+    games = collections.defaultdict(list)
+    with open_input_file() as file:
+        for line in file:
+            game_id = int(re.search(r'\d+', line)[0])
+            for many in line.partition(':')[-1].split(';'):
+                subset = collections.Counter()
+                for single in many.split(','):
+                    n, gem = single.strip().split()
+                    n = int(n)
+                    subset[gem] += n
+                games[game_id].append(subset)
+    return games
+
+
+# Initial conditions
+@memoize
+def get_initial(part=None):
+    return (bag := {'red': 12, 'green': 13, 'blue': 14})
+
+
+# Solution to part one
+@timer
+def solve_one(games, bag):
+    sum_id = 0
+    for game_id, game in games.items():
+        for subset in game:
+            if any(n > bag[gem] for gem, n in subset.items()):
+                break
+        else:
+            sum_id += game_id
+    return sum_id
+
+
+# Solution to part two
+@timer
+def solve_two(games, bag):
+    sum_power = 0
+    for game_id, game in games.items():
+        minimum = collections.Counter()
+        for subset in game:
+            for gem, n in subset.items():
+                minimum[gem] = max(minimum[gem], n)
+        sum_power += math.prod(minimum.values())
+    return sum_power
+
+
+# Solve
+print(solve_one(read(), get_initial()))
+print(solve_two(read(), get_initial()))
