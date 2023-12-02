624dbb72b9dd3e4cd7d55692fed04211837f6515
Jeppe Dakin <jeppe_dakin@hotmail.com>
2023-12-02 12:54:57 +0100

    adopt magic for day 1

--- 2023/01/main.py
+++ 2023/01/main.py
@@ -1,33 +1,51 @@
-import pathlib, sys
-sys.path.append(str(pathlib.Path(__file__).parent.parent))
-from helper import get_input_file
-
 import re
 
+import pathlib, sys
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
+from magic import *
 
-# Part one
-def solve(spelling=False):
+
+# Reading in data
+@memoize
+def read():
+    with open_input_file() as file:
+        lines = [line.strip() for line in file]
+    return lines
+
+
+# Solution to part one
+@timer
+def solve_one(lines):
+    sum_calibration = 0
+    for line in lines:
+        m = re.search(r'\d', line)[0]
+        n = re.search(r'\d', line[::-1])[0]
+        calibration = int(m + n)
+        sum_calibration += calibration
+    return sum_calibration
+
+
+# Solution to part two
+@timer
+def solve_two(lines):
     numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     values = {number: str(index) for index, number in enumerate(numbers, 1)}
     values |= {number[::-1]: str(index) for index, number in enumerate(numbers, 1)}
     pattern_digit = r'\d'
-    if spelling:
-        pattern_spelling = '|'.join(numbers)
-        pattern_forward = '{}|{}'.format(pattern_digit, pattern_spelling)
-        pattern_backward = '{}|{}'.format(pattern_digit, pattern_spelling[::-1])
-    else:
-        pattern_forward = pattern_backward = pattern_digit
+    pattern_spelling = '|'.join(numbers)
+    pattern_forward = '{}|{}'.format(pattern_digit, pattern_spelling)
+    pattern_backward = '{}|{}'.format(pattern_digit, pattern_spelling[::-1])
     regex_forward = re.compile(pattern_forward)
     regex_backward = re.compile(pattern_backward)
-    s = 0
-    with open(get_input_file()) as file:
-        for line in file:
-            m = values.get(m := re.search(regex_forward, line)[0], m)
-            n = values.get(n := re.search(regex_backward, line[::-1])[0], n)
-            s += int(m + n)
-    return s
-print('part one:', solve())
+    sum_calibration = 0
+    for line in lines:
+        m = values.get(m := re.search(regex_forward, line)[0], m)
+        n = values.get(n := re.search(regex_backward, line[::-1])[0], n)
+        calibration = int(m + n)
+        sum_calibration += calibration
+    return sum_calibration
 
-# Part two
-print('part two:', solve(spelling=True))
 
+# Solve
+print(solve_one(read()))
+print(solve_two(read()))
