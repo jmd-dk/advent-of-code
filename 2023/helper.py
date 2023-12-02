bf8716ab5bfafa8defdc40533df3b4b234e4db21
Jeppe Dakin <jeppe_dakin@hotmail.com>
2023-12-02 10:38:40 +0100

    get_input_file() should work regardless of current working directory

--- 2023/helper.py
+++ 2023/helper.py
@@ -1,5 +1,6 @@
 def get_input_file():
-    import os, pathlib
+    import inspect, os, pathlib
+    directory = pathlib.Path(inspect.stack()[1][1]).parent
     demo = os.environ.get('DEMO', '').lower()
     is_demo1 = demo in {'1', 'one', 't', 'true', 'y', 'yes'}
     is_demo2 = demo in {'2', 'two'}
@@ -7,5 +8,5 @@ def get_input_file():
     filename += 'demo-' * is_demo1
     filename += 'demo2-' * is_demo2
     filename += 'input.txt'
-    return pathlib.Path(filename)
+    return directory / filename
 
