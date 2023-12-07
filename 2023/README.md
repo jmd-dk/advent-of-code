7b5bf8ba7086f011cc613047fc3914180e73aa4b
Jeppe Dakin <jeppe_dakin@hotmail.com>
2023-12-07 05:12:55 +0100

    add comparisons to known answers

--- 2023/README.md
+++ 2023/README.md
@@ -26,8 +26,8 @@ the solutions:
   function, without the input file being read twice. As fresh copies are used,
   part two does not have to worry about the input data
   being mutated in part one.
-* `analyze()`: For easily timing the solution algorithms
-  and pretty printing the results.
+* `analyze()`: For easily timing the solution algorithms, comparing the
+  solutions to the correct answers and pretty printing the results.
 
 The magic can be removed simply by commenting out the `memoize()`
 and `analyze()` decorators and replacing the `open_input_file()` call with
@@ -38,10 +38,13 @@ without having to deal with the additional [`magic.py`](magic.py) file.
 ### Makefile
 With the [`Makefile`](Makefile) you can do the following:
 * `make`: Run all solutions.
-* `make <n>`: Run solution for day `n`.
-* `make format`: Format all solution files using the
-  [Black](https://github.com/psf/black) formatter.
-* `make format-<n>`: Format solution file for day `n`.
+  * `make <n>`: Run solution for day `n`.
+* `make format`: Format all solution files as well as [`magic.py`](magic.py)
+  using the [Black](https://github.com/psf/black) formatter.
+  * `make format-<n>`: Format solution file for day `n`.
+* `make check`: Run all solutions and compare the results to known answers.
+  * `make check-<n>`: Run solution for day `n` and compare the result to
+    known answer.
 * `make clean`: Cleanup common unwanted, auto-generated files.
 
 
--- 2023/magic.py
+++ 2023/magic.py
@@ -7,14 +7,27 @@ should import what it needs internally.
 # Function returning an open context manager
 # for the reading of the input file.
 def open_input_file():
-    """The input file is determined as follows:
+    import contextlib, inspect, pathlib
+
+    @contextlib.contextmanager
+    def open_file(path):
+        with open(path) as file:
+            yield file
+
+    directory = pathlib.Path(inspect.stack()[1][1]).parent
+    return open_file(directory / get_input_filename())
+
+
+# Function returning the input data file in accordance
+# with to the DEMO environment variable.
+def get_input_filename():
+    """The input file name is determined as follows:
     Default: input.txt
     DEMO environment variable set to 1: demo-input.txt
     DEMO environment variable set to <n> > 1: demo<2>-input.txt
     """
-    import contextlib, inspect, os, pathlib
+    import os
 
-    directory = pathlib.Path(inspect.stack()[1][1]).parent
     demo = os.environ.get('DEMO', '').lower()
     is_demo1 = demo in {'1', 'one', 't', 'true', 'y', 'yes'}
     is_demo2 = demo in {'2', 'two'}
@@ -23,13 +36,7 @@ def open_input_file():
     filename += 'demo2-' * is_demo2
     filename += f'demo{demo}-' * (not filename and demo.isdecimal())
     filename += 'input.txt'
-
-    @contextlib.contextmanager
-    def open_file(path):
-        with open(path) as file:
-            yield file
-
-    return open_file(directory / filename)
+    return filename
 
 
 # Decorator cache which always returns a fresh copy
@@ -54,23 +61,34 @@ def analyze(func):
         day: int
         part: str
         value: object
+        correct: bool
         time: float
 
         def __str__(self):
-            width = 40
+            width = 36
             esc = '\x1b'
             esc_normal = f'{esc}[0m'
             esc_gray = f'{esc}[37m'
             esc_red = f'{esc}[91m'
             esc_green = f'{esc}[92m'
+            esc_yellow = f'{esc}[93m'
             s = f'day {self.day}, part {self.part}: '
             v = str(self.value if self.value is not None else 'error')
             t = '({})'.format(pretty_time(self.time))
             spacing = ' ' * (width - len(s) - len(v) - max(len(t), 9))
             spacing += ' ' * (not spacing)
-            v = (esc_red if self.value is None else esc_green) + f'{v}{esc_normal}'
+            v = (
+                esc_red
+                if self.value is None
+                else (esc_green if self.correct else esc_yellow)
+            ) + f'{v}{esc_normal}'
+            c = {
+                None: ' ',
+                False: f'{esc_red}✘{esc_normal}',
+                True: f'{esc_green}✔{esc_normal}',
+            }[self.correct]
             t = f'{esc_gray}{t}{esc_normal}'
-            return f'{s}{v}{spacing}{t}'
+            return f'{s}{v}{spacing}{c} {t}'
 
     def pretty_time(t):
         if t <= 0:
@@ -92,6 +110,10 @@ def analyze(func):
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
+        filename_solutions = 'solutions.txt'
+        directory = pathlib.Path(inspect.getfile(func)).parent
+        day = int(directory.name)
+        part = func.__name__.split('_')[-1]
         value = None
         try:
             tic = time.perf_counter()
@@ -100,8 +122,21 @@ def analyze(func):
         except Exception:
             toc = time.perf_counter()
             traceback.print_exc()
-        day = int(pathlib.Path(inspect.getfile(func)).parent.name)
-        part = func.__name__.split('_')[-1]
-        return Solution(day, part, value, toc - tic)
+        correct = None
+        if (path := directory / filename_solutions).is_file():
+            filename_input = get_input_filename()
+            with open(path) as file:
+                for line in file:
+                    if not line.strip():
+                        continue
+                    filename, values_correct = map(str.strip, line.split(':'))
+                    if filename != filename_input:
+                        continue
+                    value_correct = (values_correct.split(',') * 2)[
+                        ['one', 'two'].index(part)
+                    ].strip()
+                    correct = str(value) == value_correct
+                    break
+        return Solution(day, part, value, correct, toc - tic)
 
     return wrapper
