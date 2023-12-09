3ca54f565e34dc2846166e3cca48fa23e89bc019
Jeppe Dakin <jeppe_dakin@hotmail.com>
2023-12-09 13:34:16 +0100

    solve day 9

---
+++ 2023/09/solve.py
@@ -0,0 +1,51 @@
+import collections, contextlib, itertools, re
+
+# Summon magic
+__import__('sys').path.append(str(__import__('pathlib').Path(__file__).parent.parent))
+from magic import *
+
+
+# Reading in data
+@memoize
+def read():
+    with open_input_file() as file:
+        histories = [list(map(int, line.split())) for line in file]
+    return histories
+
+
+# Solution to part one
+@analyze
+def solve_one(histories):
+    def extrapolate(numbers):
+        def diff(numbers):
+            return [n - m for m, n in zip(numbers, numbers[1:])]
+
+        sum_right = 0
+        while not all(n == 0 for n in numbers):
+            sum_right += numbers[-1]
+            numbers = diff(numbers)
+        return sum_right
+
+    return sum(extrapolate(history) for history in histories)
+
+
+# Solution to part two
+@analyze
+def solve_two(histories):
+    def extrapolate_left(numbers):
+        def diff(numbers):
+            return [n - m for m, n in zip(numbers, numbers[1:])]
+
+        sum_left = 0
+        sign = -1
+        while not all(n == 0 for n in numbers):
+            sum_left += (sign := -sign) * numbers[0]
+            numbers = diff(numbers)
+        return sum_left
+
+    return sum(extrapolate_left(history) for history in histories)
+
+
+# Solve
+print(solve_one(read()))
+print(solve_two(read()))
