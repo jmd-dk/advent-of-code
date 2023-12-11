2fda130fbb6c06779e8b43b5036afb8fe44be8fd
Jeppe Dakin <jeppe_dakin@hotmail.com>
2023-12-11 12:30:01 +0100

    add further demos

--- 2023/solution/11/solve.py
+++ 2023/solution/11/solve.py
@@ -1,4 +1,4 @@
-import itertools, functools, re
+import functools, re
 
 # Summon magic
 __import__('sys').path.append(str(__import__('pathlib').Path(__file__).parents[2]))
@@ -8,19 +8,26 @@ from magic import *
 # Reading in data
 @memoize
 def read():
+    factor = None
     galaxies = []
     with open_input_file() as file:
         for i, line in enumerate(file):
             for match in re.finditer(r'#', line):
                 j = match.start()
                 galaxies.append((i, j))
-    return galaxies
+            if match := re.match(r'\d+', line):
+                factor = int(match[0])
+    return galaxies, factor
 
 
 # Solution to part one
 @analyze
-def solve_one(galaxies):
-    def get_empty(row_min, row_max, dim):
+def solve_one(galaxies, factor):
+    @functools.cache
+    def get_empty(dim):
+        find_extremum = lambda ext, dim: ext(galaxy[dim] for galaxy in galaxies)
+        row_min = find_extremum(min, dim)
+        row_max = find_extremum(max, dim)
         empty = []
         for row in range(row_min + 1, row_max):
             for galaxy in galaxies:
@@ -34,6 +41,8 @@ def solve_one(galaxies):
         i0, i1 = extract_ordered(galaxy0, galaxy1, 0)
         j0, j1 = extract_ordered(galaxy0, galaxy1, 1)
         dist = (i1 - i0) + (j1 - j0)
+        rows_empty = get_empty(0)
+        cols_empty = get_empty(1)
         dist += sum(i0 < row < i1 for row in rows_empty)
         dist += sum(j0 < col < j1 for col in cols_empty)
         return dist
@@ -44,13 +53,6 @@ def solve_one(galaxies):
             i0, i1 = i1, i0
         return i0, i1
 
-    find_extremum = lambda ext, dim: ext(galaxy[dim] for galaxy in galaxies)
-    row_min = find_extremum(min, 0)
-    row_max = find_extremum(max, 0)
-    col_min = find_extremum(min, 1)
-    col_max = find_extremum(max, 1)
-    rows_empty = get_empty(row_min, row_max, 0)
-    cols_empty = get_empty(col_min, col_max, 1)
     return sum(
         get_dist(galaxy0, galaxy1)
         for index, galaxy0 in enumerate(galaxies)
@@ -60,8 +62,15 @@ def solve_one(galaxies):
 
 # Solution to part two
 @analyze
-def solve_two(galaxies):
-    def get_empty(row_min, row_max, dim):
+def solve_two(galaxies, factor):
+    if factor is None:
+        factor = 1_000_000
+
+    @functools.cache
+    def get_empty(dim):
+        find_extremum = lambda ext, dim: ext(galaxy[dim] for galaxy in galaxies)
+        row_min = find_extremum(min, dim)
+        row_max = find_extremum(max, dim)
         empty = []
         for row in range(row_min + 1, row_max):
             for galaxy in galaxies:
@@ -71,14 +80,16 @@ def solve_two(galaxies):
                 empty.append(row)
         return empty
 
-    def get_dist(galaxy0, galaxy1, expansion_factor=1_000_000):
+    def get_dist(galaxy0, galaxy1):
         i0, i1 = extract_ordered(galaxy0, galaxy1, 0)
         j0, j1 = extract_ordered(galaxy0, galaxy1, 1)
         dist = (i1 - i0) + (j1 - j0)
+        rows_empty = get_empty(0)
+        cols_empty = get_empty(1)
         expansion = 0
         expansion += sum(i0 < row < i1 for row in rows_empty)
         expansion += sum(j0 < col < j1 for col in cols_empty)
-        return dist + (expansion_factor - 1) * expansion
+        return dist + (factor - 1) * expansion
 
     def extract_ordered(galaxy0, galaxy1, dim):
         i0, i1 = galaxy0[dim], galaxy1[dim]
@@ -86,13 +97,6 @@ def solve_two(galaxies):
             i0, i1 = i1, i0
         return i0, i1
 
-    find_extremum = lambda ext, dim: ext(galaxy[dim] for galaxy in galaxies)
-    row_min = find_extremum(min, 0)
-    row_max = find_extremum(max, 0)
-    col_min = find_extremum(min, 1)
-    col_max = find_extremum(max, 1)
-    rows_empty = get_empty(row_min, row_max, 0)
-    cols_empty = get_empty(col_min, col_max, 1)
     return sum(
         get_dist(galaxy0, galaxy1)
         for index, galaxy0 in enumerate(galaxies)
@@ -101,5 +105,5 @@ def solve_two(galaxies):
 
 
 # Solve
-print(solve_one(read()))
-print(solve_two(read()))
+print(solve_one(*read()))
+print(solve_two(*read()))
