6ba7cd8d7b4dfa3a2f091d524314b9d9a5958848
Jeppe Dakin <jeppe_dakin@hotmail.com>
2024-01-04 15:48:34 +0100

    speedup using Karger's algorithm

--- 2023/solution/25/solve.py
+++ 2023/solution/25/solve.py
@@ -1,4 +1,4 @@
-import collections, heapq, re
+import collections, math, random, re
 
 # Summon magic
 __import__('sys').path.append(str(__import__('pathlib').Path(__file__).parents[2]))
@@ -15,57 +15,73 @@ def read():
             graph[component] |= set(connections)
             for connection in connections:
                 graph[connection].add(component)
+    graph = {
+        component: sorted(connections)  # sort for determinism
+        for component, connections in graph.items()
+    }
     return graph
 
 
 # Solution to part one
 @analyze
 def solve_one(graph):
-    def get_path_furthest(component):
-        visited = set()
-        visit = [(0, component)]
-        prev = {}
-        while visit:
-            dist, component = heapq.heappop(visit)
-            if component in visited:
-                continue
-            dist += 1
-            visited.add(component)
-            for connection in graph[component]:
-                if connection in visited:
+    def karger(graph):
+        def contract():
+            while True:
+                index = random.randrange(0, len(labels))
+                j = labels[index]
+                components_j = groups[j]
+                if not (connections := graph[random.choice(components_j)]):
                     continue
-                heapq.heappush(visit, (dist, connection))
-                if connection not in prev:
-                    prev[connection] = component
-        path = [component]
-        while True:
-            if (component := prev.get(component)) is None:
-                break
-            path.append(component)
-        return path
+                component_i = random.choice(connections)
+                i = labellings[component_i]
+                if i != j:
+                    break
+            labels[index] = labels[-1]
+            labels.pop()
+            group_i = groups[i]
+            group_j = groups.pop(j)
+            for component_j in components_j:
+                labellings[component_j] = i
+            cut(group_i, j)
+            cut(group_j, i)
+            group_i += components_j
 
-    def disconnect():
-        counter = collections.Counter()
-        for component in graph:
-            path = get_path_furthest(component)
-            for connection in zip(path, path[1:]):
-                counter[frozenset(connection)] += 1
-        connection = tuple(counter.most_common(1)[0][0])
-        graph[connection[0]].remove(connection[1])
-        graph[connection[1]].remove(connection[0])
+        def cut(group, j):
+            for component in group:
+                connections = set(graph[component])
+                for connection in graph[component]:
+                    if labellings[connection] == j:
+                        connections.remove(connection)
+                graph[component] = sorted(connections)  # sort for determinism
 
-    def fill():
-        visited = set()
-        visit = {next(iter(graph))}
-        while visit:
-            visited.add(component := visit.pop())
-            visit |= graph[component] - visited
-        return len(visited)
+        groups = {}
+        labellings = {}
+        for i, component in enumerate(graph):
+            groups[i] = [component]
+            labellings[component] = i
+        labels = list(groups)
+        while len(groups) > 2:
+            contract()
+        return graph, groups, labellings
 
-    for _ in range(3):
-        disconnect()
+    def is_mistaken(graph, groups, labellings, ncut):
+        for i, group in groups.items():
+            connections_ij = set()
+            for component in group:
+                for connection in graph[component]:
+                    if labellings[connection] != i:
+                        connections_ij.add(connection)
+            if len(connections_ij) != ncut:
+                return True
 
-    return (n := fill()) * (len(graph) - n)
+    random.seed('aoc')
+    ncut = 3
+    while True:
+        graph_reduced, groups, labellings = karger(graph.copy())
+        if not is_mistaken(graph_reduced, groups, labellings, ncut):
+            break
+    return math.prod(map(len, groups.values()))
 
 
 # Solve
